46e801d5346854011f56556033fc137ec4e06187
p4p1 <p4p1@vivaldi.net>
2023-11-01 17:41:10 +0100

    working prototype of design not finished yet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { HashRouter as Router } from "react-router-dom";
+import './css/index.css';
+import App from './routes/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
